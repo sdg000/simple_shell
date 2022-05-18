3f44bfac17fa32c1d2fb204fd9392c0de1c3a81f
Akosua <addo.adofowaah@gmail.com>
2022-05-18 19:44:02 +0000

    changes

--- main.h
+++ main.h
@@ -2,6 +2,13 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
 #define UNUSED(x) (void)(x)
 #define MAX_LINE 2024
 
